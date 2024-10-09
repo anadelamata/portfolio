0bc16b86a2466acda6fbeb1e67f42dcd4552fef3
anadelamata <anadelamata@hotmail.es>
2024-10-10 01:51:41 +0200

    script

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-const menu = document.querySelector("#menu");
-const nav = document.querySelector(".links");
-
-menu.onclick = () =>{
-    menu.classList.toggle('bx-x');
-    nav.classList.toggle('active');
-}
 document.addEventListener('DOMContentLoaded', () => {
-    // Seleccionar el menú y la navegación
     const menu = document.querySelector("#menu");
     const nav = document.querySelector(".links");
 
