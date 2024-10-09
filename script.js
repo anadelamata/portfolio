b2000644eadc73cda133b4f194ed1ec36df03da2
anadelamata <anadelamata@hotmail.es>
2024-10-10 01:38:08 +0200

    ArreglarEnvio

--- script.js
+++ script.js
@@ -5,36 +5,39 @@ menu.onclick = () =>{
     menu.classList.toggle('bx-x');
     nav.classList.toggle('active');
 }
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevenir que la página se recargue
+const form = document.getElementById('contact-form');
 
-    // Enviar el formulario a Formspree
-    fetch(this.action, {
-        method: this.method,
-        body: new FormData(this)
-    })
-    .then(response => {
-        if (response.ok) {
-            // Mostrar el mensaje toast
-            showToast();
+    // Escuchar el evento de envío del formulario
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevenir la acción predeterminada del formulario
 
-            // Limpiar los campos del formulario
-            this.reset(); // Limpia todos los campos del formulario
-        } else {
-            console.error('Error al enviar el formulario:', response);
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
+        // Enviar el formulario a Formspree usando fetch
+        fetch(form.action, {
+            method: form.method,
+            body: new FormData(form)
+        })
+        .then(response => {
+            if (response.ok) {
+                // Mostrar el mensaje toast
+                showToast();
+
+                // Limpiar los campos del formulario
+                form.reset(); // Limpia todos los campos del formulario
+            } else {
+                console.error('Error al enviar el formulario:', response);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
     });
-});
 
-function showToast() {
-    const toast = document.getElementById('toast');
-    toast.style.display = 'block'; // Mostrar el toast
+    function showToast() {
+        const toast = document.getElementById('toast');
+        toast.style.display = 'block'; // Mostrar el toast
 
-    // Ocultar el toast después de 3 segundos
-    setTimeout(() => {
-        toast.style.display = 'none';
-    }, 3000);
-}
+        // Ocultar el toast después de 3 segundos
+        setTimeout(() => {
+            toast.style.display = 'none';
+        }, 3000);
+    }
