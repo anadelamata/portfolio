cacf76cd5bd145baae0dee48294e949a4de4259d
anadelamata <anadelamata@hotmail.es>
2024-10-10 02:24:01 +0200

    no-cors

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector("#menu");
     const nav = document.querySelector(".links");
 
-    // Lógica del menú
     if (menu && nav) {
         menu.onclick = () => {
             menu.classList.toggle('bx-x');
@@ -10,29 +9,23 @@ document.addEventListener('DOMContentLoaded', () => {
         };
     }
 
-    // Seleccionar el formulario
     const form = document.getElementById('contact-form');
 
-    // Escuchar el evento de envío del formulario
     if (form) {
         form.addEventListener('submit', function(event) {
-            event.preventDefault(); // Prevenir la acción predeterminada del formulario
+            event.preventDefault(); 
 
-            // Enviar el formulario a Formspree usando fetch
+            
             fetch(form.action, {
                 method: form.method,
-                body: new FormData(form)
+                body: new FormData(form),
+                mode: 'no-cors' 
             })
             .then(response => {
-                if (response.ok) {
-                    // Mostrar el mensaje toast
-                    showToast();
+               
+                showToast();
 
-                    // Limpiar los campos del formulario
-                    form.reset(); // Limpia todos los campos del formulario
-                } else {
-                    console.error('Error al enviar el formulario:', response);
-                }
+                form.reset(); 
             })
             .catch(error => {
                 console.error('Error:', error);
@@ -42,15 +35,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function showToast() {
         const toast = document.getElementById('toast');
-        toast.style.display = 'block'; // Mostrar el toast
-        toast.style.opacity = '1'; // Asegúrate de que la opacidad sea visible
+        toast.style.display = 'block'; 
+        toast.style.opacity = '1'; 
 
-        // Ocultar el toast después de 3 segundos
+       
         setTimeout(() => {
-            toast.style.opacity = '0'; // Desvanecer el toast
+            toast.style.opacity = '0'; 
             setTimeout(() => {
-                toast.style.display = 'none'; // Ocultar después de desvanecer
-            }, 500); // Tiempo de desvanecimiento
-        }, 3000); // Mostrar durante 3 segundos
+                toast.style.display = 'none'; 
+            }, 500); 
+        }, 3000); 
     }
 });
