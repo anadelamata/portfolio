7e549cb6f410c94e3362a0c641e760c4216fccb3
anadelamata <anadelamata@hotmail.es>
2024-10-10 03:13:54 +0200

    post

--- script.js
+++ script.js
@@ -9,23 +9,4 @@
     }
 
 
-    const btn = document.getElementById('button');
-
-  document.getElementById('form')
-  .addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    btn.value = 'Sending...';
-
-    const serviceID = 'default_service';
-    const templateID = 'template_dm0y2ew';
-
-    emailjs.sendForm(serviceID, templateID, this)
-      .then(() => {
-        btn.value = 'Send Email';
-        alert('Sent!');
-      }, (err) => {
-        btn.value = 'Send Email';
-        alert(JSON.stringify(err));
-      });
-  });
+    
